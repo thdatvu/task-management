7b09933bdd93ddaefed3540e99579378e64eaeb0
thdatvu <thanhdatvu1009@gmail.com>
2025-01-05 08:12:48 +0700

    Loc theo trang thai

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,31 @@
+const Task = require("../../../models/task.model");
+
+//[GET] api/v1/tasks
+module.exports.index = async(req, res) => {
+    const find = {
+        deleted: false
+    }
+    console.log(req.query);
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+    const tasks = await Task.find(find);
+    
+    res.json(tasks);
+};
+//[GET] api/v1/tasks/detail/:id
+module.exports.detail = async(req, res) => {
+    try {
+        const id = req.params.id;
+        
+        const tasks = await Task.findOne({
+            _id: id,
+            deleted : false
+        })
+       
+        res.json(tasks);
+    }catch{
+        res.json("Not found");
+        
+    }
+};
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-//const controller = require("../../controllers/client/task.controller");
+const controller = require("../controllers/task.controller");
 
 const Task = require("../../../models/task.model");
-router.get("/", async(req, res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    console.log(tasks);
-    res.json(tasks);
-});
+router.get("/", controller.index);
 
-router.get("/detail/:id", async(req, res) => {
-    try {
-        const id = req.params.id;
-        
-        const tasks = await Task.findOne({
-            _id: id,
-            deleted : false
-        })
-        console.log(tasks)
-        res.json(tasks);
-    }catch{
-        res.json("Not found");
-        
-    }
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
