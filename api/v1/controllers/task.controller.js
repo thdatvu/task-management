16ba05c33f45ac47e2b7ebf4ee4c66543dd18a40
thdatvu <thanhdatvu1009@gmail.com>
2025-01-05 08:44:00 +0700

    Pagination

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,4 +1,5 @@
 const Task = require("../../../models/task.model");
+const paginationHelper = require("../../../helpers/pagination")
 
 //[GET] api/v1/tasks
 module.exports.index = async(req, res) => {
@@ -8,12 +9,27 @@ module.exports.index = async(req, res) => {
     if(req.query.status) {
         find.status = req.query.status;
     }
+    //Pagination
+    let initPagination = {
+        currentPage:1,
+        limitItems:2
+    }
+    const countTasks = await Task.countDocuments(find);
+    let objectPagination = paginationHelper(
+        initPagination,
+        req.query,
+        countTasks
+    );
+    //End pagination
     //Sort
     const sort = {};
     if(req.query.sortKey && req.query.sortValue){
         sort[req.query.sortKey] = req.query.sortValue
     }
-    const tasks = await Task.find(find).sort(sort);
+    const tasks = await Task.find(find)
+        .sort(sort)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
     //End Sort
     res.json(tasks);
 };
