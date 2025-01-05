de953448ee0951713c1c7fa566f06059e9292138
thdatvu <thanhdatvu1009@gmail.com>
2025-01-05 15:27:48 +0700

    Delelte one or multi

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -97,6 +97,18 @@ module.exports.changeMulti = async (req, res) => {
                     message: "Cập nhật trạng thái thành công!"
                 });
                 break;
+            case "delete":
+                await Task.updateMany({
+                    _id: {$in: ids}
+                },{
+                    deleted: true,
+                    deletedAt: new Date()
+                });
+                res.json({
+                    code: 200,
+                    message: "Xoa thành công!"
+                });
+                break;
             default:
                 res.json({
                     code: 400,
@@ -142,6 +154,26 @@ module.exports.edit = async (req, res) => {
         res.json({
         code: 400,
         message: "Lỗi!"
+        });
+    }
+};
+
+// [DELETE] /api/v1/tasks/delete/:id
+module.exports.delete = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Task.updateOne({ _id: id }, {
+        deleted: true,
+        deletedAt: new Date()
+    });
+    res.json({
+        code: 200,
+        message: "Xóa thành công!"
+    });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Loil"
         });
     }
 };
